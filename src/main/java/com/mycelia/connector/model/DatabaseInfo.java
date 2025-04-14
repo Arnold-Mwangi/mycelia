b0b0cb8d3e6386302faef8a526c24d68c11683f0
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2025-04-14 04:16:38 +0300

    Data Access Objects

---
+++ src/main/java/com/mycelia/connector/model/DatabaseInfo.java
@@ -0,0 +1,25 @@
+package com.mycelia.connector.model;
+
+public class DatabaseInfo {
+    String name;
+    String version;
+    String vendor;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    public String getVendor() {
+        return vendor;
+    }
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+}
---
+++ src/main/java/com/mycelia/connector/model/QuerySession.java
@@ -0,0 +1,4 @@
+package com.mycelia.connector.model;
+
+public class QuerySession {
+}
